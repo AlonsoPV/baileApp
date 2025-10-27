d77ab36da69025453c5dcd1451f860f7846fca82
AlonsoPV <alpeva96@gmail.com>
2025-10-27 13:31:36 -0600

    debug: Add authentication loading logs

--- apps/web/src/AppRouter.tsx
+++ apps/web/src/AppRouter.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import AppShell from './components/layout/AppShell';
 import { routes } from './routes/registry';
 
 // Guards
@@ -70,121 +71,58 @@ import RouteDiagnostics from './dev/RouteDiagnostics';
 export default function AppRouter() {
   return (
     <Routes>
-        {/* Auth Routes - Redirect if authenticated */}
-        <Route
-          path={routes.auth.login}
-          element={
-            <RedirectIfAuthenticated>
-              <Login />
-            </RedirectIfAuthenticated>
-          }
-        />
-        <Route
-          path={routes.auth.signup}
-          element={
-            <RedirectIfAuthenticated>
-              <Signup />
-            </RedirectIfAuthenticated>
-          }
-        />
+      {/* Auth */}
+      <Route path={routes.auth.login} element={<Login />} />
+      <Route path={routes.auth.signup} element={<Signup />} />
 
-        {/* Onboarding Routes */}
-        <Route path={routes.onboarding.basics} element={<ProfileBasics />} />
-        <Route path={routes.onboarding.ritmos} element={<PickRitmos />} />
-        <Route path={routes.onboarding.zonas} element={<PickZonas />} />
+      {/* Onboarding public */}
+      <Route path={routes.onboarding.basics} element={<ProfileBasics />} />
+      <Route path={routes.onboarding.ritmos} element={<PickRitmos />} />
+      <Route path={routes.onboarding.zonas} element={<PickZonas />} />
 
-        {/* Protected Routes with OnboardingGate */}
+      {/* AppShell layout */}
+      <Route element={<AppShell />}>
+        {/* Public */}
+        <Route path="/explore" element={<ExploreHomeScreen />} />
+        <Route path="/explore/list" element={<ExploreListScreen />} />
+        <Route path="/organizer/:id" element={<OrganizerPublicScreen />} />
+        <Route path="/organizador/:organizerId" element={<OrganizerPublicScreen />} />
+        <Route path="/profile/organizer" element={<OrganizerProfileLiveNew />} />
+        <Route path="/academia/:academyId" element={<AcademyPublicScreen />} />
+        <Route path="/profile/academy" element={<AcademyProfileLive />} />
+        <Route path="/marca/:brandId" element={<BrandPublicScreen />} />
+        <Route path="/u/:userId" element={<UserPublicProfile />} />
+
+        {/* Protected */}
         <Route element={<OnboardingGate />}>
-          {/* App Routes */}
           <Route path={routes.app.profile} element={<ProfileScreen />} />
-          <Route path={routes.app.explore} element={<ExploreHomeScreen />} />
-          <Route path="/explore/list" element={<ExploreListScreen />} />
-          
-          {/* Info Screen */}
-          <Route path="/info" element={<InfoScreen />} />
-          
-          {/* User Profile Routes */}
           <Route path="/profile" element={<ProfileScreen />} />
           <Route path="/profile/edit" element={<ProfileScreen />} />
           <Route path="/profile/settings" element={<ProfileScreen />} />
-          
-          {/* Organizer Routes */}
           <Route path={routes.organizer.edit} element={<OrganizerEditScreen />} />
           <Route path="/profile/organizer/edit" element={<OrganizerProfileEditor />} />
-          
-          {/* Event Routes */}
           <Route path="/events/parent/new" element={<EventParentEditScreen />} />
           <Route path="/events/parent/:id/edit" element={<EventParentEditScreen />} />
           <Route path="/events/date/new/:parentId" element={<EventDateEditScreen />} />
           <Route path="/events/date/:id/edit" element={<EventDateEditScreen />} />
-          
-          {/* Social/Event Flow Routes */}
           <Route path="/social/new" element={<EventParentEditScreen />} />
           <Route path="/social/:parentId/edit" element={<EventParentEditScreen />} />
           <Route path="/social/:parentId/fecha/nueva" element={<EventDateEditScreen />} />
           <Route path="/social/fecha/:dateId/edit" element={<EventDateEditScreen />} />
-          
-          {/* My RSVPs */}
           <Route path="/me/rsvps" element={<MyRSVPsScreen />} />
-          
-          {/* Academy Routes */}
           <Route path={routes.academy.edit} element={<AcademyEditorScreen />} />
           <Route path="/profile/academy/edit" element={<AcademyProfileEditor />} />
-          
-          {/* Brand Routes */}
           <Route path={routes.brand.edit} element={<BrandEditorScreen />} />
-          
-          {/* Role Selection */}
           <Route path="/profile/roles" element={<RoleSelectorScreen />} />
-          
-          {/* Admin Routes */}
           <Route path="/admin/roles" element={<AdminRoleRequestsScreen />} />
-          
-          {/* Debug Routes */}
           <Route path="/debug/integrity" element={<IntegrityDebugScreen />} />
         </Route>
 
-        {/* Public Routes (no authentication required) */}
-        
-        {/* Organizer Public */}
-        <Route path="/organizador/:organizerId" element={<OrganizerPublicScreen />} />
-        <Route path="/profile/organizer" element={<OrganizerProfileLiveNew />} />
-        
-        {/* Academy Public */}
-        <Route path="/academia/:academyId" element={<AcademyPublicScreen />} />
-        <Route path="/profile/academy" element={<AcademyProfileLive />} />
-        
-        {/* Brand Public */}
-        <Route path="/marca/:brandId" element={<BrandPublicScreen />} />
-        
-        {/* User Public */}
-        <Route path="/u/:userId" element={<UserPublicProfile />} />
-        <Route path="/profile/user/edit" element={<UserProfileEditor />} />
-        
-        {/* Event Live Routes */}
-        <Route path="/evento/:parentId" element={<EventParentPublicScreen />} />
-        <Route path="/evento/fecha/:dateId" element={<EventDatePublicScreen />} />
-        <Route path="/social/:id" element={<SocialLiveScreen />} />
-        <Route path="/social/fecha/:id" element={<DateLiveScreen />} />
-        
-        {/* Legacy event routes */}
-        <Route path="/events/parent/:id" element={<EventParentPublicScreen />} />
-        <Route path="/events/date/:id" element={<EventDatePublicScreen />} />
-
-        {/* System Routes */}
-        <Route path={routes.misc.unauthorized} element={<Unauthorized />} />
-        <Route path={routes.misc.notFound} element={<NotFound />} />
-
-        {/* Dev-only Route Diagnostics */}
-        {import.meta.env.DEV && (
-          <Route path="/__routes" element={<RouteDiagnostics />} />
-        )}
-
-        {/* Default redirect */}
-        <Route path={routes.root} element={<Navigate to={routes.app.profile} replace />} />
-
-        {/* Catch all - 404 */}
+        {/* Default and 404 */}
+        <Route path="/" element={<Navigate to="/explore" replace />} />
         <Route path="*" element={<NotFound />} />
-      </Routes>
+      </Route>
+    </Routes>
   );
 }
+
