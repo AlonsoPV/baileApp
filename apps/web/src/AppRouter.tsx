78e3008ffeb460d54f4531dc094f8da7d0f5a246
AlonsoPV <alpeva96@gmail.com>
2025-11-04 16:11:07 -0600

    debug: Add authentication loading logs

--- apps/web/src/AppRouter.tsx
+++ apps/web/src/AppRouter.tsx
@@ -76,19 +76,11 @@ import AdminRoleRequestsScreen from './screens/admin/AdminRoleRequestsScreen';
 import RequestRoleScreen from './screens/roles/RequestRoleScreen';
 import IntegrityDebugScreen from './screens/debug/IntegrityDebugScreen';
 
-// Dev-only components
-import RouteDiagnostics from './dev/RouteDiagnostics';
-import PinSetup from './screens/auth/PinSetup';
-import PinLogin from './screens/auth/PinLogin';
-import AuthCallback from './screens/auth/AuthCallback';
-
 // Challenges
 import ChallengesList from './screens/challenges/ChallengesList';
 import ChallengeNew from './screens/challenges/ChallengeNew';
 import ChallengeDetail from './screens/challenges/ChallengeDetail';
 import ClassPublicScreen from './screens/classes/ClassPublicScreen';
-import OrganizerPublicLive from './screens/profile/OrganizerPublicLive';
-import AcademyPublicLive from './screens/profile/AcademyPublicLive';
 import TeacherPublicLive from './screens/profile/TeacherPublicLive';
 
 export default function AppRouter() {
@@ -114,14 +106,14 @@ export default function AppRouter() {
         <Route path="/quienes-somos" element={<QuienesSomosScreen />} />
         <Route path="/about" element={<AboutScreen />} />
         <Route path="/legal" element={<LegalScreen />} />
-        <Route path="/organizer/:id" element={<OrganizerPublicLive />} />
-        <Route path="/organizador/:organizerId" element={<OrganizerPublicLive />} />
+        <Route path="/organizer/:id" element={<OrganizerPublicScreen />} />
+        <Route path="/organizador/:organizerId" element={<OrganizerPublicScreen />} />
         <Route path="/social/:id" element={<EventParentPublicScreenModern />} />
         <Route path="/social/fecha/:id" element={<EventDatePublicScreen />} />
         <Route path="/profile/organizer" element={<OrganizerProfileLiveNew />} />
         <Route path="/profile/organizer/:id" element={<OrganizerPublicScreen />} />
-        <Route path="/academia/:academyId" element={<AcademyPublicLive />} />
-        <Route path="/profile/academy/:academyId" element={<AcademyPublicLive />} />
+        <Route path="/academia/:academyId" element={<AcademyPublicScreen />} />
+        <Route path="/profile/academy/:academyId" element={<AcademyPublicScreen />} />
         <Route path="/profile/academy" element={<AcademyProfileLive />} />
         <Route path="/profile/teacher" element={<TeacherProfileLive />} />
         <Route path="/u/:userId" element={<UserPublicScreen />} />
